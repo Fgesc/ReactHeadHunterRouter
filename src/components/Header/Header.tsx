2fb8c08c40a7a83200f6e42a296aec76389f5fa2
Artem Neij <artemneij@gmail.com>
2025-11-05 21:02:59 +0300

    Link to NavLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,29 @@
 import { Box, Group, Text, useMantineTheme } from "@mantine/core";
-import { Link, useLocation} from 'react-router-dom';
+import { NavLink, useLocation} from 'react-router-dom';
 import HumanIcon from "../../assets/human.svg"
+import HumanIconBlack from "../../assets/humanBlack.svg"
 import HHIcon from "../../assets/HHunter.png"
 //Logo - намеренно в png, svg не хочет билдиться, ни напрямую ни через импорт
 export const Header = () => {
     const theme = useMantineTheme();
 
     const location = useLocation();
-    const isCurrent = location.pathname === '/vacancies';
+    const isCurrent = location.pathname === '/vacancies/moscow';
 
     const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
         if (isCurrent) e.preventDefault();
     };
+
+    const Marker = () => (
+        <Box
+            style={{
+                width: 6,
+                height: 6,
+                borderRadius: "50%",
+                backgroundColor: "#4263EB",
+            }}
+        />
+    );
 
     return (
         <Box    
@@ -25,46 +37,42 @@ export const Header = () => {
             }}
         >
             <Group justify="space-between" align="center" maw={828}>
-                <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }} onClick={handleClick}>
+                <NavLink to="/vacancies/" style={{ textDecoration: 'none', color: 'inherit' }} onClick={handleClick}>
                     <Group align="center" gap="xs">
                         <img src={HHIcon} alt="HH" data-testid="logo-hh"/>
                         <Text fw={600} data-testid="header-title">.FrontEnd</Text>
                     </Group>
-                </Link>
+                </NavLink>
 
                 <Group gap={24}>
-                    <Link to="/vacancies" style={{ textDecoration: 'none', color: 'inherit' }}   onClick={handleClick}>
+                    <NavLink to="/vacancies/" style={({ isActive }) => ({color: isActive ? theme.other.colors.black1 : theme.other.colors.gray, textDecoration: "none"})} onClick={handleClick}>
+                        {({ isActive }) => (
+                            <Group align="center" gap={8}>
+                                <Text
+                                    data-testid="vacancies-text"
+                                    fw={500}
+                                    style={{
+                                        cursor: "pointer",
+                                    }}
+                                >
+                                    Вакансии FE
+                                </Text>
+                                {isActive && <Marker />}
+                            </Group>
+                        )}
+                    </NavLink>
 
-                        <Group align="center" gap={8}>
-                            <Text
-                                data-testid="vacancies-text"
-                                fw={500}
-                                style={{
-                                    cursor: "pointer",
-                                }}
-                            >
-                                Вакансии FE
-                            </Text>
-                            <Box
-                                data-testid="vacancies-marker"
-                                style={{
-                                    width: 6,
-                                    height: 6,
-                                    borderRadius: "50%",
-                                    backgroundColor: "#4263EB",
-                                }}
-                            />
-                        </Group>
-                    </Link>
-
-                    <Link to="/aboutme" style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <Group align="center" gap={6} style={{ cursor: "pointer" }}>
-                            <img src={HumanIcon} alt="human" data-testid="human-icon"/>
-                            <Text fw={500} c={theme.other.colors.gray} data-testid="about-text">
-                                Обо мне
-                            </Text>
-                        </Group>
-                    </Link>
+                    <NavLink to="/aboutme" style={({ isActive }) => ({color: isActive ? theme.other.colors.black1 : theme.other.colors.gray, textDecoration: "none"})}>
+                        {({ isActive }) => (
+                            <Group align="center" gap={6} style={{ cursor: "pointer" }}>
+                                <img src={isActive ? HumanIconBlack : HumanIcon} alt="human" data-testid="human-icon"/>
+                                <Text fw={500} data-testid="about-text">
+                                    Обо мне
+                                </Text>
+                                {isActive && <Marker />}  
+                            </Group>
+                        )}
+                    </NavLink>
                 </Group>
             </Group>
         </Box>
